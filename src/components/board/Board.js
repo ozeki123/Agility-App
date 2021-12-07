4610374aa7923192e3709f93aa0102228100747d
Andrew Ozeki <andrewozeki@Andrews-MacBook-Pro.local>
2021-12-07 23:42:52 +0900

    created: board component

---
+++ src/components/board/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Board.scss'
+
+const Board = () =>
+{
+    return(
+        <div className="board-container">
+            <h1>Board works!</h1>
+        </div>
+    )
+}
+
+export default Board
